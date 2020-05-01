6335db2cf2cf81835a6c7d8414630a2fd5b77296
335g <actionstar619@yahoo.co.jp>
2020-05-01 23:47:26 +0900

    Impl simple view

--- 01/01/ContentView.swift
+++ 01/01/ContentView.swift
@@ -3,8 +3,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var b1: Bool = false
+    @State var b2: Bool = true
+    @State var b3: Bool = true
+    
     var body: some View {
-        Text("Hello, World!")
+        let action1 = {
+            self.b1 = true
+            self.b2 = false
+        }
+        let action2 = {
+            self.b2 = true
+            self.b3 = false
+        }
+        let action3 = {
+            self.b3 = true
+            self.b1 = false
+        }
+        
+        return HStack {
+            Button("Button 1", action: action1).disabled(b1)
+            Button("Button 2", action: action2).disabled(b2)
+            Button("Button 3", action: action3).disabled(b3)
+        }
+            .padding()
             .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
