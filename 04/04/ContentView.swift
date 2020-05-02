dd43928af4e228323ebc7fac63f655891a33fcfa
335g <actionstar619@yahoo.co.jp>
2020-05-03 01:38:55 +0900

    Impl (TextFied in ForEach cannot edit the content)

--- 04/04/ContentView.swift
+++ 04/04/ContentView.swift
@@ -1,11 +1,25 @@
 //  Copyright © 2020 335g. All rights reserved.
 
 import SwiftUI
+import Combine
+
+// MARK: - ViewModel
+
+final class ContentViewModel: ObservableObject {
+    @Published var texts: [String] = ["a", "b", "c", "d"]
+}
+
+// MARK: - View
 
 struct ContentView: View {
+    @ObservedObject var viewModel: ContentViewModel = .init()
+    
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        List {
+            ForEach(viewModel.texts.indices, id: \.self) { idx in
+                MyTextField(text: self.$viewModel.texts[idx])
+            }
+        }
     }
 }
 
