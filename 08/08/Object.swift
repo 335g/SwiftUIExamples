dbb9d8b2ea751a3ebe18716bc740796cf7f09e21
335g <actionstar619@yahoo.co.jp>
2020-05-06 12:29:12 +0900

    Add 08

---
+++ 08/08/Object.swift
@@ -0,0 +1,17 @@
+//  Copyright © 2020 335g. All rights reserved.
+
+import SwiftUI
+
+struct Object: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Object_Previews: PreviewProvider {
+    static var previews: some View {
+        Object()
+    }
+}
